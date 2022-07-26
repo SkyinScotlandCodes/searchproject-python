32ab161b2394b4b7c4a7af5476041a1fed8b9833
Kat <93621189+Lunoctopuss@users.noreply.github.com>
2022-07-26 18:04:29 +0100

    Update searchproject.py

--- searchproject.py
+++ searchproject.py
@@ -1,11 +1,9 @@
 import requests
-import config
 
 
 def recipe_search(ingredient):
-    # Register to get an APP ID and key https://developer.edamam.com/
-    app_id = config.application_id
-    app_key = config.application_key
+    app_id = '7b009ee0'
+    app_key = '5929f995b6fda7c2e998943fa9abc48e'
     result = requests.get('https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key))
     data = result.json()
     return data['hits']
@@ -14,11 +12,11 @@ def recipe_search(ingredient):
 def run():
     ingredient = input('Enter an ingredient: ')
     results = recipe_search(ingredient)
+
     for result in results:
         recipe = result['recipe']
         print(recipe['label'])
         print(recipe['uri'])
         print()
-
 
 run()
