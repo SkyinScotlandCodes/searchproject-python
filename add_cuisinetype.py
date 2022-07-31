8d96b8edb8395653a9b86bafd1088ab5c3203a0b
Skyinscotlandcodes <susie.y123@hotmail.co.uk>
2022-07-31 20:21:19 +0100

    update

--- add_cuisinetype.py
+++ add_cuisinetype.py
@@ -1,14 +1,15 @@
 CuisineType_array = {"American", "British", "Caribbean", "Chinese", "French", "Italian", "Japanese", "Kosher",
                      "Mediterranean", "Mexican"}
 
-CuisineType_array = [CuisineType_array.lower() for CuisineType_array in CuisineType_array]
+# apply lower case to CuisineType_array
+# CuisineType_array = [CuisineType_array.lower() for CuisineType_array in CuisineType_array]
 
 # removed from CuisineType_array
 # "Asian" "Central Europe"  "Eastern Europe" "Middle Eastern" "Nordic" "South American" "South East Asian"
 
 # ask user to enter cuisine preference
 inputCuisineType = input(
-    f"Please enter choose a preferred cuisine from the following options below or type 'N' if none - \n{CuisineType_array}: ")
+    f"Invalid Response. Please enter choose a preferred cuisine from the following options below or type 'N' if none - \n{CuisineType_array}: ")
 while (inputCuisineType != "N" and inputCuisineType != "n") and inputCuisineType not in CuisineType_array:
     inputCuisineType = input(
         f"Please enter choose a preferred cuisine from the following options below or type 'N' if none - \n{CuisineType_array}: ")
