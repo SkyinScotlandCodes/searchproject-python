58b4bf7a56a6f7bc0cc3c591cf6de058d61eeaa7
Skyinscotlandcodes <susie.y123@hotmail.co.uk>
2022-07-31 21:36:41 +0100

    remove type 'N' if none

--- searchproject.py
+++ searchproject.py
@@ -13,8 +13,6 @@ endPagination = "to=10"
 CuisineType_array = {"American", "British", "Caribbean", "Chinese", "French", "Italian", "Japanese", "Kosher",
                      "Mediterranean", "Mexican"}
 
-CuisineType_array = [CuisineType_array.lower() for CuisineType_array in CuisineType_array]
-
 # ask user to enter ingredient(s)
 inputIngredient = input("Please enter one or more ingredients to search for: ")
 # return invalid response if user enters nothing or only spaces
@@ -28,8 +26,8 @@ addIngredients = inputIngredient
 
 # ask user to enter cuisine preference
 inputCuisineType = input(
-    f"Please enter choose a preferred cuisine from the following options below or type 'N' if none - \n{CuisineType_array}: ")
-while (inputCuisineType != "N" and inputCuisineType != "n") and inputCuisineType not in CuisineType_array:
+    f"Please enter choose a preferred cuisine from the following options below - \n{CuisineType_array}: ")
+while inputCuisineType not in CuisineType_array:
     inputCuisineType = input(
         f"Invalid Response. Please enter choose a preferred cuisine from the following options below or type 'N' if none - \n{CuisineType_array}: ")
 # print("You have chosen: " + inputCuisineType)
@@ -38,12 +36,15 @@ addCuisineType = inputCuisineType
 
 print(f'You have searched for {inputCuisineType} recipes using {inputIngredient}')
 
-
 r = requests.get(
     f"https://api.edamam.com/search?q={addIngredients}&cuisineType={addCuisineType}&{includeAppId}&{includeAppKey}&{startPagination}&{endPagination}")
 
 data = r.json()
 results = data['hits']
+
+count = data['count']
+
+print(f"{count} recipes found")
 
 for result in results:
     recipe = result['recipe']
